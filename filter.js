a2d6f62dd3a1d87f4d2acb273619e030ec2c8bfa
michalklimt <56630544+michalklimt@users.noreply.github.com>
2023-12-04 18:35:45 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,87 +1,74 @@
 // filter.js
-var csvArray = [];
-var categoryIndex = -1;
-var categories = new Set();
-var selectedCategory = null; // For tracking the selected category
+document.addEventListener("DOMContentLoaded", function() {
+    var csvArray = [];
+    var categoryIndex = -1;
+    var categories = new Set();
+    var selectedCategory = null;
 
-function loadCSV() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            parseCSV(xhr.responseText);
-        }
-    };
-    xhr.open(
-        "GET",
-        "https://michalklimt.github.io/UItootling-experiments/Tooling-list.csv", // Make sure the path is correct
-        true,
-    );
-    xhr.send();
-}
-
-function parseCSV(csv) {
-    var lines = csv.split("\\n");
-    var headers = lines[0].split(",");
-    categoryIndex = headers.indexOf("Category");
-    csvArray = lines.slice(1).map((line) => line.split(","));
-    displayCSV(csvArray);
-    extractCategories();
-    displayTags();
-}
-
-function displayCSV(dataArray) {
-    var html = "<table border='1'>";
-    dataArray.forEach(function (row) {
-        html += "<tr>";
-        row.forEach(function (cell) {
-            html += "<td>" + cell.trim() + "</td>";
-        });
-        html += "</tr>";
-    });
-    html += "</table>";
-    document.getElementById("csv-data").innerHTML = html;
-}
-
-function extractCategories() {
-    csvArray.forEach((row) => {
-        if (row[categoryIndex]) categories.add(row[categoryIndex].trim());
-    });
-}
-
-function displayTags() {
-    var tagsContainer = document.getElementById("tags-container");
-    categories.forEach((category) => {
-        var tagButton = document.createElement("button");
-        tagButton.textContent = category;
-        tagButton.classList.add('category-button');
-        tagButton.onclick = function () {
-            selectCategory(category);
-            filterCategory(category);
-        };
-        tagsContainer.appendChild(tagButton);
-    });
-}
-
-function selectCategory(category) {
-    selectedCategory = category;
-    document.querySelectorAll('.category-button').forEach(button => {
-        if (button.textContent === category) {
-            button.classList.add('selected');
-        } else {
-            button.classList.remove('selected');
-        }
-    });
-}
-
-function filterCategory(category) {
-    if (category) {
-        var filteredData = csvArray.filter(
-            (row) => row[categoryIndex] === category,
-        );
-        displayCSV(filteredData);
-    } else {
-        displayCSV(csvArray); // Show all if no category is selected
+    function loadCSV() {
+        fetch('https://michalklimt.github.io/UItootling-experiments/Tooling-list.csv')
+            .then(response => response.text())
+            .then(data => parseCSV(data))
+            .catch(error => console.error('Error fetching CSV:', error));
     }
-}
 
-window.onload = loadCSV;
+    function parseCSV(csv) {
+        var lines = csv.split("\n");
+        var headers = lines[0].split(",");
+        categoryIndex = headers.indexOf("Category");
+        csvArray = lines.slice(1).map(line => line.split(","));
+        displayCSV(csvArray);
+        extractCategories();
+        displayTags();
+    }
+
+    function displayCSV(dataArray) {
+        var html = "<table><thead><tr>";
+        dataArray[0].forEach(header => {
+            html += "<th>" + header + "</th>";
+        });
+        html += "</tr></thead><tbody>";
+        dataArray.slice(1).forEach(row => {
+            html += "<tr>";
+            row.forEach(cell => {
+                html += "<td>" + cell + "</td>";
+            });
+            html += "</tr>";
+        });
+        html += "</tbody></table>";
+        document.getElementById("csv-data").innerHTML = html;
+    }
+
+    function extractCategories() {
+        csvArray.forEach(row => {
+            if (row[categoryIndex]) categories.add(row[categoryIndex].trim());
+        });
+    }
+
+    function displayTags() {
+        var tagsContainer = document.getElementById("tags-container");
+        categories.forEach(category => {
+            var tagButton = document.createElement("button");
+            tagButton.textContent = category;
+            tagButton.addEventListener('click', function() {
+                selectCategory(category);
+                filterCategory(category);
+            });
+            tagsContainer.appendChild(tagButton);
+        });
+    }
+
+    function selectCategory(category) {
+        selectedCategory = category;
+        document.querySelectorAll('button').forEach(button => {
+            button.classList.toggle('selected', button.textContent === category);
+        });
+    }
+
+    function filterCategory(category) {
+        var filteredData = csvArray.filter(row => row[categoryIndex] === category);
+        displayCSV(filteredData);
+    }
+
+    loadCSV();
+});
